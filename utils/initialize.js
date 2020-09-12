c7fa38da3784236a5a23cfe7ed1f276cbd51a675
darianelourdes <darianelsantana@gmail.com>
2020-09-11 22:45:58 -0300

    feat: helper for initialization

---
+++ utils/initialize.js
@@ -0,0 +1,16 @@
+import Router from 'next/router';
+import actions from '../redux/actions';
+import { getCookie } from './cookie';
+
+export default function(ctx) {
+  if(ctx.isServer) {
+    ctx.store.dispatch(actions.reauthenticate(getCookie("token", ctx, req)));
+  } else if(ctx.store) {
+    const token = ctx.store.getState().auth.token;
+    if(!token && (
+        ctx.pathname.includes("/customer-area/request") ||
+        ctx.pathname.includes("/customer-area/data") ||
+        ctx.pathname.includes("/customer-area/change-password") 
+    )) Router.push("/customer-area");
+  }
+}
