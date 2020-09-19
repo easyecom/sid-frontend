7801b6f8404026925a119f81ae723fca86d2a6d0
darianelourdes <darianelsantana@gmail.com>
2020-09-18 22:28:15 -0300

    feat: creating the store header

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,22 @@ import React, { Component } from 'react';
 
 import Layout from '../components/Layout';
 
-// import Cabecalho from '../containers/Cabecalho';
-// import Banners from '../containers/Banners';
-// import Beneficios from '../containers/Beneficios';
-// import ProdutosPaginaInicial from '../containers/ProdutosPaginaInicial';
-// import Rodape from '../containers/Rodape';
+import Cabecalho from '../containers/Cabecalho';
+import Banners from '../containers/Banners';
+import Beneficios from '../containers/Beneficios';
+import ProdutosPaginaInicial from '../containers/ProdutosPaginaInicial';
+import Rodape from '../containers/Rodape';
 
 
 export default class Index extends Component {
   render() {
     return(
       <Layout title="Sid Surf Store | Estilo e bom gosto">
-        <h2>Sid Surf Store</h2>
+        <Cabecalho />
+        <Banners />
+        <Beneficios />
+        <ProdutosPaginaInicial />
+        <Rodape />
       </Layout>
       
     )
