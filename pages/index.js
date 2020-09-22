cf2349ec944273f9640fd5f5b4c403b307d98e0d
darianelourdes <darianelsantana@gmail.com>
2020-09-22 20:13:26 -0300

    feat: home page style

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 import Layout from '../components/Layout';
 
 import Cabecalho from '../containers/Cabecalho';
-import Banners from '../containers/Banners';
-import Beneficios from '../containers/Beneficios';
-import ProdutosPaginaInicial from '../containers/ProdutosPaginaInicial';
-import Rodape from '../containers/Rodape';
+// import Banners from '../containers/Banners';
+// import Beneficios from '../containers/Beneficios';
+// import ProdutosPaginaInicial from '../containers/ProdutosPaginaInicial';
+// import Rodape from '../containers/Rodape';
 
 
 class Index extends Component {
@@ -14,11 +14,11 @@ class Index extends Component {
   render() {
     return (
       <Layout title="Sid Surf Store | Estilo e bom gosto">
-        <Cabecalho simples />
-        <Banners />
+        <Cabecalho />
+        {/* <Banners />
         <Beneficios />
         <ProdutosPaginaInicial />
-        <Rodape />
+        <Rodape /> */}
       </Layout>
       
     )
