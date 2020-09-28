dc1349d80025812389e05db7c204454ae05ad42d
darianelourdes <darianelsantana@gmail.com>
2020-09-28 20:45:20 -0300

    feat: card style

--- containers/Lista/ProdutosPaginaInicial/index.js
+++ containers/Lista/ProdutosPaginaInicial/index.js
@@ -5,29 +5,29 @@ import Produtos from '../../../components/Listas/Produtos';
 const PRODUTOS = [
   {
     id: 93856,
-    fotos: ["/static/img/mouse-1.png"],
-    titulo: "Mouse Gamer 1",
-    preco: 35,
-    promocao: 25
+    fotos: ["/static/img/shirt-oakley.jpg"],
+    titulo: "Camiseta Oakley",
+    preco: 129,
+    promocao: 109
   },
   {
     id: 99056,
-    fotos: ["/static/img/mouse-4.png"],
-    titulo: "Mouse Gamer 2 haifiuay kiufio kdhiigpdp",
+    fotos: ["/static/img/short-green.jpg"],
+    titulo: "Short Estampado",
     preco: 55,
     promocao: 55
   },
   {
     id: 95456,
-    fotos: ["/static/img/mouse-5.png"],
-    titulo: "Mouse Gamer 3",
-    preco: 105,
-    promocao: 95
+    fotos: ["/static/img/tenis-nike-air-max.jpg"],
+    titulo: "Tenis da nike air max",
+    preco: 599,
+    promocao: 429
   },
   {
     id: 97056,
-    fotos: ["/static/img/mouse-2.png"],
-    titulo: "Mouse Gamer 4",
+    fotos: ["/static/img/meia-stance.jpg"],
+    titulo: "Meia stance",
     preco: 160,
     promocao: 150
   },
