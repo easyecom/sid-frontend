3591a97be9cd5d9d4855d5461a97d46191d341d1
darianelourdes <darianelsantana@gmail.com>
2020-11-03 22:04:21 -0300

    feat: creating success page

--- pages/success.js
+++ pages/success.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 
+import Layout from '../components/Layout';
+import Cabecalho from '../containers/Cabecalho';
+import SucessoContainer from '../containers/Sucesso';
+import Rodape from '../containers/Rodape';
+
 export default class Success extends Component {
   render() {
     return(
-      <div>
-        <h2>Loja SID - Sucesso</h2>
-      </div>
-    )
-  }
-}
+      <Layout title="LOJA SID | SURF STORE">
+        <Cabecalho simples />
+        <SucessoContainer />
+        <Rodape />
+      </Layout>
+    );
+  };
+};
