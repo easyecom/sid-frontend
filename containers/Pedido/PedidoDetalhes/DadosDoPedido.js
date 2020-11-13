805244c24ecd842072b6e4394ef618324e27300a
darianelourdes <darianelsantana@gmail.com>
2020-11-12 23:57:06 -0300

    feat: creating components of the order details page

--- containers/Pedido/PedidoDetalhes/DadosDoPedido.js
+++ containers/Pedido/PedidoDetalhes/DadosDoPedido.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
+import { formatMoney } from '../../../utils';
 import TextoDados from '../../../components/Texto/Dados';
+import TabelaSimples from '../../../components/Tabela/Simples';
 
 
 class DadosDoPedido extends Component {
@@ -17,6 +19,61 @@ class DadosDoPedido extends Component {
       </div>
     );
   };
+
+  renderDadosDoCarrinho() {
+    const carrinho = [
+      {
+        "Produto": "Meia Stance - Tam. Unico",
+        "Preço Und": formatMoney(55),
+        "Quantidade": 1,
+        "Preço Total": formatMoney(55)
+      },
+      {
+        "Produto": "Short Green - M",
+        "Preço Und": formatMoney(105),
+        "Quantidade": 1,
+        "Preço Total": formatMoney(105)
+      }
+    ];
+    return (
+      <div className="flex-5">
+        <h4 className="headline">CARRINHO</h4>
+        <br/>
+        <TabelaSimples
+          cabecalho={[ "Produto", "Preço Und.", "Quantidade", "Preço Total" ]}
+          dados={carrinho} />
+      </div>
+    );
+  };
+
+  renderDadosDeEntrega() {
+    return (
+      <div className="flex-3">
+        <h4 className="headline">DADOS DE ENTREGA</h4>
+        <br/>
+        <TextoDados chave="Endereço" valor="Rua teste" />
+        <TextoDados chave="Número" valor="123" />
+        <TextoDados chave="Bairro" valor="Centro" />
+        <TextoDados chave="Complemento" valor="" />
+        <TextoDados chave="Cidade" valor="Mogi das Cruzes" />
+        <TextoDados chave="Estado" valor="SP" />
+        <TextoDados chave="CEP" valor="12345-678" />
+      </div>
+    );
+  };
+
+  renderDadosDePagamento() {
+    return (
+      <div className="flex-5">
+        <h4 className="headline">DADOS DE PAGAMENTO</h4>
+        <br/>
+        <TextoDados chave="Taxa de Entrega" valor={formatMoney(20)} />
+        <TextoDados chave="Valor do Pedido" valor={formatMoney(160)} />
+        <TextoDados chave="Valor Total" valor={formatMoney(180)} />
+        <TextoDados chave="Forma de Pagamento" valor="Cartão de Crédito - 2x" />
+      </div>
+    );
+  }
 
   render() {
     return (
--- containers/Pedido/index.js
+++ containers/Pedido/index.js
@@ -6,7 +6,7 @@ import PedidoDetalhes from './PedidoDetalhes';
 class PedidoContainer extends Component {
   render() {
     return (
-      <div className="Pedido-Container container-big felx horizontal">
+      <div className="Pedido-Container container-big flex horizontal">
         <MenuAreaDoCliente />
         <PedidoDetalhes />
       </div>
