675bbe8e1fdf0b3e8d4b2cbaa2604ead6ae2b4ce
darianelourdes <darianelsantana@gmail.com>
2020-12-03 23:18:45 -0300

    refactor: footer integration

--- components/Item/DadosDaLoja.js
+++ components/Item/DadosDaLoja.js
@@ -6,27 +6,20 @@ class DadosDaLoja extends React.Component {
 
   render() {
     if (!this.props.loja) return (<div></div>);
-    const { loja } = this.props
+    const [ loja ]  = this.props.loja
+    
     return (
       <div className="flex-1 dados-da-loja">
         <div>
           <h2>Entre em Contato</h2>
           <br />
         </div>
-
-        {
-          loja.map(lojas => (
-            <>
-              <p className="loja-nome">Nome: {lojas.storeName}</p>
-              <p className="loja-cnpj">CNPJ: {lojas.cnpj}</p>
-              <p className="loja-nome">E-mail: <a href={`mailto:${lojas.email}`}>{lojas.email}</a></p>
-              <p className="loja-telefones">Telefones: <a href={`phone:${lojas.cellPhone}`}>{lojas.cellPhone}</a></p>
-              {/* <p className="loja-endereço">{endereco.local}, {endereco.numero} - {endereco.bairro}</p>
-              <p className="loja-cidade">{endereco.cidadade}/{endereco.estado} - {endereco.cep}</p> */}
-            </>
-          ))
-        }
-
+          <p className="loja-nome">Nome: {loja.storeName}</p>
+          <p className="loja-cnpj">CNPJ: {loja.cnpj}</p>
+          <p className="loja-nome">E-mail: <a href={`mailto:${loja.email}`}>{loja.email}</a></p>
+          <p className="loja-telefones">Telefones: <a href={`phone:${loja.cellPhone}`}>{loja.cellPhone}</a></p>
+          {/* <p className="loja-endereço">{endereco.local}, {endereco.numero} - {endereco.bairro}</p>
+                <p className="loja-cidade">{endereco.cidadade}/{endereco.estado} - {endereco.cep}</p> */}
       </div>
     )
   }
