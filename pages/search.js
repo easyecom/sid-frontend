c51ab6c29118e45cd97d5a383d9a269c2cdda334
darianelourdes <darianelsantana@gmail.com>
2021-02-10 22:20:01 -0300

    feat: page search - added actions and reducers for integration

--- pages/search.js
+++ pages/search.js
@@ -5,10 +5,25 @@ import Cabecalho from '../containers/Cabecalho';
 import ProdutosPesquisa from '../containers/Lista/ProdutosPesquisa';
 import Rodape from '../containers/Rodape';
 
-export default class Search extends Component {
+import initialize from '../utils/initialize';
+import callBaseData from '../utils/callBaseData';
+
+import { connect } from 'react-redux';
+import actions from '../redux/actions';
+
+class Search extends Component {
+  static async getInitialProps(ctx) {
+    initialize(ctx);
+    if(ctx.store) ctx.store.dispatch(actions.fetchTermo(ctx.query.termo));
+    return callBaseData([
+      actions.fetchProdutosCategoria.bind(null, ctx.query.termo)
+    ], ctx);
+  }
+
   render() {
+    const { termo } = this.props;
     return(
-      <Layout title="Resultados para | SID SURF STORE">
+      <Layout title={`Resultados para ${termo ? termo: "-"} | SID SURF STORE`}>
         <Cabecalho />
         <ProdutosPesquisa />
         <Rodape />
@@ -16,3 +31,9 @@ export default class Search extends Component {
     );
   };
 };
+
+const mapStateToProps = state => ({
+  termo: state.produto.termo
+})
+
+export default connect(mapStateToProps, actions)(Search);
--- redux/actions/produtoActions.js
+++ redux/actions/produtoActions.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import {
-  FETCH_PRODUTOS
+  FETCH_PRODUTOS,
+  FETCH_PESQUISA,
+  FETCH_PRODUTOS_PESQUISA
 } from '../types';
 import { API, loja } from '../../config';
 
@@ -11,7 +13,17 @@ export const fetchProdutosPaginaInicial = () => (dispatch) => {
   .catch(e => console.log(e));
 }
 
+export const fetchTermo = (termo) => ({ type: FETCH_PESQUISA, termo });
+
+export const fetchProdutosPesquisa = (termo, atual, limit) => dispatch => {
+  axios.get(`${API}/stores/${loja}/products/search/${termo}?page=${atual}&limit=${limit}`)
+  .then((response) => dispatch({ type: FETCH_PRODUTOS_PESQUISA, payload: response.data, termo }))
+  .catch(e => console.log(e));
+}
+
 
 export default {
-  fetchProdutosPaginaInicial
+  fetchProdutosPaginaInicial,
+  fetchTermo,
+  fetchProdutosPesquisa
 };
