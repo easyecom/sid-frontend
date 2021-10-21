1f8c3dfb13ca65dc4a8fbe14bfb4f14bbc10e9ae
thiago carvalho <thiagocarvalho.ads@gmail.com>
2021-10-20 23:39:00 -0300

    wip for vuild

--- pages/checkClient.js
+++ pages/checkClient.js
@@ -1,20 +1,18 @@
-// import React, { Component } from "react";
+import React, { Component } from "react";
 
-// import Layout from "../components/Layout";
+import Layout from "../components/Layout";
 
-// import Cabecalho from "../containers/Cabecalho";
-// import CheckClient from "../containers/Carrinho/CheckClient";
-// import CheckClient from "../containers/Carrinho";
-// import Rodape from "../containers/Rodape";
+import Cabecalho from "../containers/Cabecalho";
+import Rodape from "../containers/Rodape";
 
-// export default class CheckClientPage extends Component {
-//   render() {
-//     return (
-//       <Layout title="Ckeckout | LOJA SID SURF STORE">
-//         <Cabecalho simples title={"Validação de CPF"} />
-//         <CheckClient />
-//         <Rodape />
-//       </Layout>
-//     );
-//   }
-// }
+export default class CheckClientPage extends Component {
+  render() {
+    return (
+      <Layout title="Ckeckout | LOJA SID SURF STORE">
+        <Cabecalho simples title={"Validação de CPF"} />
+        
+        <Rodape />
+      </Layout>
+    );
+  }
+}
