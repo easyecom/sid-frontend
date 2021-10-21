18e1e14eff172ac17f0bc589383bf7572b54b331
thiago carvalho <thiagocarvalho.ads@gmail.com>
2021-10-20 23:52:57 -0300

    remove module not found

--- pages/checkClient.js
+++ pages/checkClient.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import Layout from "../components/Layout";
 
 import Cabecalho from "../containers/Cabecalho";
-import CheckClient from "../containers/Carrinho/CheckClient";
+// import CheckClient from "../containers/Carrinho/CheckClient";
 import Rodape from "../containers/Rodape";
 
 export default class CheckClientPage extends Component {
@@ -11,7 +11,7 @@ export default class CheckClientPage extends Component {
     return (
       <Layout title="Ckeckout | LOJA SID SURF STORE">
         <Cabecalho simples title={"Validação de CPF"} />
-        <CheckClient />
+        {/* <CheckClient /> */}
         <Rodape />
       </Layout>
     );
