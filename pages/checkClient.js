b01c3a0c3b333a9ac29e7e2d71f13f88e96466a5
thiago carvalho <thiagocarvalho.ads@gmail.com>
2021-10-20 23:26:02 -0300

    remove check clientes

--- pages/checkClient.js
+++ pages/checkClient.js
@@ -1,19 +1,20 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
 
-import Layout from "../components/Layout";
+// import Layout from "../components/Layout";
 
-import Cabecalho from "../containers/Cabecalho";
-import CheckClient from "../containers/Carrinho/CheckClient";
-import Rodape from "../containers/Rodape";
+// import Cabecalho from "../containers/Cabecalho";
+// import CheckClient from "../containers/Carrinho/CheckClient";
+// import CheckClient from "../containers/Carrinho";
+// import Rodape from "../containers/Rodape";
 
-export default class CheckClientPage extends Component {
-  render() {
-    return (
-      <Layout title="Ckeckout | LOJA SID SURF STORE">
-        <Cabecalho simples title={"Validação de CPF"} />
-        <CheckClient />
-        <Rodape />
-      </Layout>
-    );
-  }
-}
+// export default class CheckClientPage extends Component {
+//   render() {
+//     return (
+//       <Layout title="Ckeckout | LOJA SID SURF STORE">
+//         <Cabecalho simples title={"Validação de CPF"} />
+//         <CheckClient />
+//         <Rodape />
+//       </Layout>
+//     );
+//   }
+// }
