bff977f8e20b94b1a52ff2ba6f566eaa5ad64e68
thiago carvalho <thiagocarvalho.ads@gmail.com>
2021-10-29 19:25:55 -0300

    fefature: initial responsive menu

--- components/menuResponsive/index.js
+++ components/menuResponsive/index.js
@@ -1,24 +1,106 @@
 import React, { Component } from "react";
 import Link from "next/link";
+import { connect } from "react-redux";
+import actions from "../../redux/actions";
+
+const categories = [
+  {
+    category: "vestuarios",
+    categories: [
+      { categoryChildren: "camisetas" },
+      { categoryChildren: "blusas" },
+      { categoryChildren: "bermudas" },
+      { categoryChildren: "calças" },
+    ],
+  },
+  {
+    category: "calcados",
+    categories: [
+      { categoryChildren: "tenis" },
+      { categoryChildren: "sapatos" },
+      { categoryChildren: "botinhas" },
+      { categoryChildren: "chinelos" },
+    ],
+  },
+  {
+    category: "acessorios",
+    categories: [
+      { categoryChildren: "relogios" },
+      { categoryChildren: "bones" },
+      { categoryChildren: "correntes" },
+      { categoryChildren: "perfumes" },
+    ],
+  },
+  {
+    category: "eletronicos",
+    categories: [
+      { categoryChildren: "celulares" },
+      { categoryChildren: "smartWatch" },
+      { categoryChildren: "carregadores" },
+    ],
+  },
+];
 
 class MenuResposive extends Component {
-  state = {};
+  state = {
+    open: false,
+  };
 
   componentDidMount() {}
 
   componentDidUpdate() {}
 
+  toggleOpen = () => this.setState({ open: !this.state.open });
+
+  handleMenuResponsive() {
+    console.log(this.props, "props");
+    return (
+      <div className="Menu-responsive">
+        <div className="header-menu-responsive">
+          <p>
+            <Link href="/customer-area">
+              <span className="login-responsive">Entre</span>
+            </Link>{" "}
+            ou{" "}
+            <Link href="/customer-area">
+              <span
+                onClick={this.props.changeAcesso}
+                className="register-responsive"
+              >
+                cadastre-se
+              </span>
+            </Link>
+          </p>
+
+          <p onClick={() => this.toggleOpen()}>
+            <i className="fa fa-angle-left"></i>
+          </p>
+        </div>
+
+        <h3>Categorias</h3>
+        <div>
+          {categories.map((e) => (
+            <div>{e.category}</div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+
   render() {
+    const { open } = this.state;
+
     return (
       <div className="menu-responsive">
         <div>
-          <Link href="/customer-area">
+          {open ? this.handleMenuResponsive() : ""}
+          <div onClick={() => this.toggleOpen()}>
             <i className="fa fa-bars"></i>
-          </Link>
+          </div>
         </div>
       </div>
     );
   }
 }
 
-export default MenuResposive;
+export default connect(null, actions)(MenuResposive);
